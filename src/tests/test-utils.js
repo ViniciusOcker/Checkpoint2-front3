d594cbd382f6eb1023e69143248a9b1d94357920
RodrigoGottschall <ro_criscuolo@hotmail.com>
2022-12-15 20:24:06 -0300

    feat tests init

--- src/tests/test-utils.js
+++ src/tests/test-utils.js
@@ -1,34 +1,52 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { BrowserRouter, MemoryRouter, Routes, Route } from "react-router-dom"
-import { ContextProvider } from "../Components/utils/global.context"
+import { GlobalProvider } from "../hooks/globalContext"
 import Home from "../Routes/Home"
 import Detail from "../Routes/Detail"
 import Login from "../Routes/Login"
 import App from "../App"
 
+const API_URL = 'https://dhodonto.ctdprojetos.com.br'
 
-const renderWithContext = (ui, providerValue)=>{
+const renderWithContext = (ui, providerValue) => {
     return render(
         <BrowserRouter>
-            <ContextProvider value={providerValue || {theme: "light", data: []}} >   
+            <GlobalProvider value={providerValue || { theme: "dark", auth: '', api: API_URL }} >
                 {ui}
-            </ContextProvider>
+            </GlobalProvider>
         </BrowserRouter>
-    )
+    )   
 }
 
-//Only for testing individual routes as /dentist/:id
-export const renderWithRouter = (ui, {route = '/', path='/'}) => {
+describe('Navbar componente', () => {
+    it('Deveria ser renderizado em tela', () => {
+        renderWithContext(<App />)
+
+        expect(screen.getByRole('banner')).toHaveTextContent('DH Odonto')
+    })
+});
+  
+describe('Footer componente', () => {
+    it('Deveria ser renderizado em tela', () => {
+        renderWithContext(<App />)
+
+        expect(screen.getByRole('contentinfo')).toHaveTextContent('Voltar para o topo')
+    })
+});  
+
+
+// Only for testing individual routes as /dentist/:id
+export const renderWithRouter = (ui, { route = '/', path = '/' }) => {
     window.history.pushState({}, 'Test page', route)
 
     return render(
         <MemoryRouter initialEntries={[route]}>
             <Routes>
-                <Route index path={path} element={ui}/>
+                <Route index path={path} element={ui} />
             </Routes>
         </MemoryRouter>
     )
 }
 
 export * from "@testing-library/react"
-export {renderWithContext as render}  
+export { renderWithContext as render }  
